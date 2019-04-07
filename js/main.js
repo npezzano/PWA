dc46221a3bd2744e618f5a7bad5c2379425fb985
npezzano <48226762+npezzano@users.noreply.github.com>
2019-04-07 14:07:37 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,5 +4,6 @@ window.onload = () => {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
              .register('./sw.js');
-  }
+			 .then(function() { console.log("Service Worker Registered"); });
+}
 }
